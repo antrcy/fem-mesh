9ef877afaedaf8931733f3f1d441f67bae188252
antrcy <antoine.ruch@etu.unistra.fr>
2025-03-14 17:52:33 +0100

    moving code

--- include/meshclass.hpp
+++ include/meshclass.hpp
@@ -219,4 +219,43 @@ public:
     int exportToVTK(const std::string& path, const std::string& plotName, const functionType& function = 0);
 };
 
+
+class FunctionSpace {
+    public:
+    
+        FunctionSpace(const Mesh& mesh): M_domain(mesh) {
+            M_globalDof = M_domain.getNbNodes();
+        }
+    
+        unsigned int connectivity(int elementId, int localIndex) const;
+        
+        class FunctionElement {
+        public:
+            FunctionElement(const FunctionSpace& funcSpace) : M_functionSpace(funcSpace) {
+                M_data.resize(funcSpace.M_globalDof);
+            }
+    
+            /** @brief Sets all values to zero. */
+            void setZero();
+            /** @brief Evaluate an expression over each dof. */
+            void evaluate(functionType expression);
+            /** @brief Sets a certain value given a node Id. */
+            void setValue(int nodeId, double value);
+            /** @brief Sets an element's node to a certain value. */
+            void setValue(int elementId, int localIndex, double value);
+            /** @brief Returns a value given a node Id. */
+            double getValue(int nodeId) const;
+    
+        private:
+            const FunctionSpace& M_functionSpace;
+            std::vector<double> M_data;
+        };
+    
+        FunctionElement element() const {return FunctionElement(*this);}
+    
+    private:
+        const Mesh& M_domain;
+        unsigned int M_globalDof;
+    };
+
 #endif
--- source/meshclass.cpp
+++ source/meshclass.cpp
@@ -540,3 +540,43 @@ int Mesh::exportToVTK(const std::string& path, const std::string& plotName, cons
     ofile.close();
     return 0;
 }
+
+/**
+ * FUNCTIONSPACE CLASS IMPLEMENTATION
+*/
+
+unsigned int FunctionSpace::connectivity(int elementId, int localIndex) const {
+    int globalIndex = (M_domain.getElement(elementId)).globalNodeIndex[localIndex];
+    return globalIndex;
+}
+
+// FUNCTION ELEMENT
+
+void FunctionSpace::FunctionElement::setZero() {
+    int n = M_functionSpace.M_globalDof;
+    for (int i = 0; i < n; i ++){
+        M_data[i] = 0;
+    }
+}
+
+void FunctionSpace::FunctionElement::evaluate(functionType expression) {
+    const Mesh& domain = M_functionSpace.M_domain;
+
+    for (const auto& index : domain.idToIndexNodes){
+        const Node& point = domain.getNode(index.first);
+
+        M_data[index.second] = expression(point(0), point(1));
+    }
+}
+
+void FunctionSpace::FunctionElement::setValue(int id, double value) {
+    M_data[M_functionSpace.M_domain.idToIndexNodes.at(id)] = value;
+}
+
+void FunctionSpace::FunctionElement::setValue(int elementId, int localIndex, double value) {
+    M_data[M_functionSpace.connectivity(elementId, localIndex)] = value;
+}
+
+double FunctionSpace::FunctionElement::getValue(int id) const {
+    return M_data[M_functionSpace.M_domain.idToIndexNodes.at(id)];
+}
